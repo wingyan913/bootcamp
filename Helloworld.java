5d53c320026a73fb42a18a7638e4b3cb85f04014
Wingyan913 <conliwingyan@gmail.com>
2023-07-16 01:40:19 +0800

    ignore files

--- Helloworld.java
+++
@@ -1,9 +0,0 @@
-// allow comment only (out of {})
-public class Helloworld {
-  public static void main (String[] args){
-    // print out something
-    System.out.println("Hello, on9."); // end with semi-colon
-    
-  
-  }
-}
